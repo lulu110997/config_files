b5e09c655f04d8a5e2371790c55ed82c0438a7a4
lulu110997 <louis97fernandez@gmail.com>
2024-01-31 18:07:35 +1100

    hpc script

---
+++ hpc_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir /scratch/lffernan/ && cd /scratch/lffernan/
+cp /home/lffernan/tensorflow-2.6.0-gpu-py3.sif /scratch/lffernan/
+singularity shell --nv tensorflow-2.6.0-gpu-py3.sif
+cp /home/lffernan/.mpose/ /scratch/lffernan/ -r
+export HOME=/scratch/lffernan/ && export PATH=$PATH:/scratch/lffernan/.local/bin
+echo "############ copy AcT to this dir and cd AcT && pip install -r requirements.txt ############"
