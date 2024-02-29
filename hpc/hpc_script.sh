dba46a21fdf6f7c914260a66348e1dd57639739b
lulu110997 <louis97fernandez@gmail.com>
2024-02-29 19:55:37 +1100

    add command to run python script and move folder to home when done

--- hpc/hpc_script.sh
+++ hpc/hpc_script.sh
@@ -5,3 +5,4 @@ singularity shell --nv tensorflow-2.6.0-gpu-py3.sif
 cp /home/lffernan/.mpose/ /scratch/lffernan/ -r
 export HOME=/scratch/lffernan/ && export PATH=$PATH:/scratch/lffernan/.local/bin
 echo "############ copy AcT to this dir and cd AcT && pip install -r requirements.txt ############"
+nohup sh -c "python -u main.py && cp ./* /home/lffernan/output/ -r" > output.txt 2>&1 &
