289691ff5aef96adfbbc1be8c13b801ebc975be6
lulu110997 <53894562+lulu110997@users.noreply.github.com>
2024-03-03 16:54:15 +1100

    Update hpc_script.sh

--- hpc/hpc_script.sh
+++ hpc/hpc_script.sh
@@ -5,4 +5,5 @@ singularity shell --nv tensorflow-2.6.0-gpu-py3.sif
 cp /home/lffernan/.mpose/ /scratch/lffernan/ -r
 export HOME=/scratch/lffernan/ && export PATH=$PATH:/scratch/lffernan/.local/bin
 echo "############ copy AcT to this dir and cd AcT && pip install -r requirements.txt ############"
-nohup sh -c "python -u main.py && cp ./* /home/lffernan/output/ -r" > output.txt 2>&1 &
+nohup sh -c "python -u main.py && cp ./* /home/lffernan/output/ -r" 2>&1 output.txt &
+watch -n2 "clear && tail -50 nohup.out"
