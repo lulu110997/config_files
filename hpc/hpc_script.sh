7fb329bbc4702eea691795ed88e717e4c7229ef2
louis-fernandez-UTS <louis.f.fernandez@student.uts.edu.au>
2024-03-08 15:26:00 +1100

    add some commands

--- hpc/hpc_script.sh
+++ hpc/hpc_script.sh
@@ -7,3 +7,5 @@ export HOME=/scratch/lffernan/ && export PATH=$PATH:/scratch/lffernan/.local/bin
 echo "############ copy AcT to this dir and cd AcT && pip install -r requirements.txt ############"
 nohup sh -c "python -u main.py && cp ./* /home/lffernan/output/ -r" 2>&1 output.txt &
 watch -n2 "clear && tail -50 nohup.out"
+# runs in background, still doesn't create nohup.out
+nohup command > nohup.out 2>&1 &  
